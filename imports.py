a9c6386d84b350ecbbc91bbf7755c150d6ec8423
ShirilMahato <108653062+ShirilMahato@users.noreply.github.com>
2023-08-11 22:18:55 +0545

    Add files via upload

--- imports.py
+++ imports.py
@@ -1,9 +1,28 @@
 import sys
-from PyQt5.QtCore import Qt, QEvent
-from PyQt5.QtGui import QColor, QPalette, QPixmap, QLinearGradient, QPainter
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QMessageBox, QHBoxLayout, QFrame
+import os
+import shutil
+from PyQt5.QtCore import Qt, QEvent, QDir, QFileInfo, QUrl, QPropertyAnimation, QRect, QMetaObject
+from PyQt5.QtGui import QColor, QPalette, QPixmap, QLinearGradient, QPainter, QFont, QIcon, QDesktopServices
+
+from PyQt5.QtWidgets import (
+    QApplication, QMainWindow, QLabel, QLineEdit, QPushButton,
+    QTextEdit, QVBoxLayout, QWidget, QFormLayout, QSizePolicy,
+    QFileDialog, QMessageBox, QHBoxLayout, QTabWidget, QFrame,
+    QPushButton, QListWidget, QListWidgetItem, QStackedWidget,
+    QInputDialog, QFileDialog, QTextBrowser
+)
+from PyQt5.QtTest import QTest
+import time
+import cryptography
+from cryptography.fernet import Fernet, InvalidToken
+import base64
+from unittest.mock import patch, MagicMock
+
+
+from dashboard import ChatHeaderWidget
 from login import LoginWindow
 from signup import SignupWindow
 from mainwindow import MainWindow
-import bcrypt
-import sqlite3
+from encryption import EncryptionManager
+from contact_functions import ContactFunctions
+from chat_functions import ChatFunctions
